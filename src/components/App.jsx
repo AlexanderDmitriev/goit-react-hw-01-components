baf50d68d328084f32e35610bce8421a94c77690
AlexanderDmitriev <alex9110077@gmail.com>
2022-03-18 23:43:43 +0200

    START

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,13 @@
+import { Profile } from "./Profile";
+import user from '../';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+      />
+
   );
 };
