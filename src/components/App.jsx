61883bc348ecdbd7132b7ff5a619095bcf744014
AlexanderDmitriev <alex9110077@gmail.com>
2022-03-20 19:18:30 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,34 @@
 import { Profile } from "./Profile";
-import user from '../';
+import { Container } from "./App.styled";
+//import user from '';
+
+const user ={
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308
+  }
+};
 
 export const App = () => {
   return (
+    <Container>
       <Profile
+        key={user.tag}
         username={user.username}
         tag={user.tag}
         location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
       />
+    </Container>
+      
 
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,11 @@
-export const Profile =({username,tag,location,avatar,stats}) =>{
+import PropTypes from 'prop-types';
+
+export const Profile =({username,tag,location,avatar="https://cdn-icons-png.flaticon.com/512/1077/1077012.png",followers,views,likes}) =>{
     return (
         <div className="profile">
             <div className="description">
                 <img
-                src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+                src={avatar}
                 alt="User avatar"
                 className="avatar"
                 />
@@ -15,17 +17,27 @@ export const Profile =({username,tag,location,avatar,stats}) =>{
             <ul className="stats">
                 <li>
                 <span className="label">Followers</span>
-                <span className="quantity">1000</span>
+                <span className="quantity">{followers}</span>
                 </li>
                 <li>
                 <span className="label">Views</span>
-                <span className="quantity">2000</span>
+                <span className="quantity">{views}</span>
                 </li>
                 <li>
                 <span className="label">Likes</span>
-                <span className="quantity">3000</span>
+                <span className="quantity">{likes}</span>
                 </li>
             </ul>
         </div>
     );
-}
+};
+
+Profile.propTypes={
+        username:PropTypes.string.isRequired,
+        tag:PropTypes.string.isRequired,
+        location:PropTypes.string,
+        avatar:PropTypes.string,
+        followers:PropTypes.number,
+        views:PropTypes.number,
+        likes:PropTypes.number
+};
